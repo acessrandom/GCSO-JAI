e1f983784a7d0161a43f967eded48257064f7892
acessrandom <75856494+acessrandom@users.noreply.github.com>
2021-03-24 18:53:58 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var wall = createSprite(200,100)
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
